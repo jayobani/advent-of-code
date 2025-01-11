b46bdca92aa4d1c76e45f53e3855543cfdd54714
James Obaniyi <jobaniyi65@gmail.com>
2025-01-11 06:59:51 -0800

    Day 5 of Advent Of Code 2024

---
+++ aoc_2024/aoc_2024_05_01.py
@@ -0,0 +1,30 @@
+from itertools import chain
+from collections import defaultdict
+
+dg = defaultdict(set)
+ul = list()
+with open("aoc_2024_05_input.txt") as file:
+    ip = 0
+    for l in file:
+        if(l.rstrip() == ""):
+            ip += 1
+        elif(ip == 0):
+            t = tuple(map(int, l.rstrip().split("|")))
+            dg[t[1]].add(t[0])
+        elif(ip == 1):
+            ul.append(list(map(int, l.rstrip().split(","))))
+    
+result = 0
+for u in ul:
+    o = [0] * len(u)
+    us = set(u)
+    for n in us:
+        i = len(us & dg[n])
+        if(n != u[i]):
+            break
+        o[i] = n
+    else:
+        result += u[len(u) // 2]
+print(result)
+            
+                
